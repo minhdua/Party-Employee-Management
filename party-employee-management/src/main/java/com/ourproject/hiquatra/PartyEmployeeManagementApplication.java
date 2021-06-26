696bc681b2ee333988f09c2e8c0ca2cfeb0257a2
HienVo <vothanhhienag1996@gmail.com>
2021-06-26 17:33:18 +0700

    [HIEN]-Create projects

---
+++ party-employee-management/src/main/java/com/ourproject/hiquatra/PartyEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ourproject.hiquatra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyEmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ party-employee-management/src/test/java/com/ourproject/hiquatra/PartyEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourproject.hiquatra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyEmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
